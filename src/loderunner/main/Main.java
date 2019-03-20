65b0692815e2f264d396bcf99443b33e58f51630
Basile Pesin <basile.pesin@gmail.com>
2019-03-20 19:34:02 +0100

    Added build.xml and appropriate basic files

---
+++ src/loderunner/main/Main.java
@@ -0,0 +1,7 @@
+package loderunner.main;
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO
+    }
+}
---
+++ src/loderunner/test/Tests.java
@@ -0,0 +1,12 @@
+package loderunner.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        // TODO
+    })
+
+public class Tests {}
