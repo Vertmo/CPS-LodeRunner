f405309bd98d35b978092cfcc245a824874aefdb
Basile Pesin <basile.pesin@gmail.com>
2019-03-24 17:47:44 +0100

    Added bad pixel-art

--- src/loderunner/windows/LevelCanvas.java
+++ src/loderunner/windows/LevelCanvas.java
@@ -4,6 +4,7 @@ import java.util.Set;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
 import loderunner.io.Level;
@@ -11,7 +12,17 @@ import loderunner.services.Coord;
 import loderunner.services.Screen;
 
 class LevelCanvas extends Canvas {
-    public final static int CELL_W = 20;
+    public static final int CELL_W = 32;
+
+    private static final Image pltImg = new Image("assets/img/plt.png", (double) CELL_W, (double) CELL_W, true, true);
+    private static final Image ladImg = new Image("assets/img/lad.png", (double) CELL_W, (double) CELL_W, true, true);
+    private static final Image hdrImg = new Image("assets/img/hdr.png", (double) CELL_W, (double) CELL_W, true, true);
+    private static final Image mtlImg = new Image("assets/img/mtl.png", (double) CELL_W, (double) CELL_W, true, true);
+
+    private static final Image playerImg = new Image("assets/img/player.png", (double) CELL_W, (double) CELL_W, true, true);
+    private static final Image guardImg = new Image("assets/img/guard.png", (double) CELL_W, (double) CELL_W, true, true);
+    private static final Image treasureImg = new Image("assets/img/treasure.png", (double) CELL_W, (double) CELL_W, true, true);
+    private static final Image treaGuardImg = new Image("assets/img/treasureGuard.png", (double) CELL_W, (double) CELL_W, true, true);
 
     @Override
     public boolean isResizable() {
@@ -35,45 +46,49 @@ class LevelCanvas extends Canvas {
     }
 
     public void drawCells(Screen s) {
-        // TODO du pixel art
         GraphicsContext gc = getGraphicsContext2D();
+        gc.setFill(Color.BLACK);
+        gc.fillRect(0, 0, s.getWidth()*CELL_W, s.getHeight()*CELL_W);
         for(int x = 0; x < s.getWidth(); x++) {
             for(int y = 0; y < s.getHeight(); y++) {
                 switch(s.getCellNature(x, y)) {
-                case EMP: gc.setFill(Color.BLACK); break;
-                case PLT: gc.setFill(Color.BROWN); break;
-                case HOL: gc.setFill(Color.BLACK); break;
-                case LAD: gc.setFill(Color.YELLOW); break;
-                case HDR: gc.setFill(Color.GREEN); break;
-                case MTL: gc.setFill(Color.GRAY); break;
+                case EMP: break;
+                case HOL: break;
+                case PLT:
+                    gc.drawImage(pltImg, x*CELL_W, (s.getHeight()-1-y)*CELL_W);
+                    break;
+                case LAD:
+                    gc.drawImage(ladImg, x*CELL_W, (s.getHeight()-1-y)*CELL_W);
+                    break;
+                case HDR:
+                    gc.drawImage(hdrImg, x*CELL_W, (s.getHeight()-1-y)*CELL_W);
+                    break;
+                case MTL:
+                    gc.drawImage(mtlImg, x*CELL_W, (s.getHeight()-1-y)*CELL_W);
+                    break;
                 }
-                gc.fillRect(x*CELL_W, (s.getHeight()-1-y)*CELL_W, CELL_W, CELL_W);
             }
         }
     }
 
     public void drawContents(Screen s, Coord pc, Set<Coord> gCoords, Set<Coord> tCoords) {
-        // TODO du pixel art
         GraphicsContext gc = getGraphicsContext2D();
+
         // Draw player
-        gc.setFill(Color.GREEN);
-        gc.fillOval(pc.getCol()*CELL_W,
-                    (s.getHeight()-1-pc.getHgt())*CELL_W,
-                    CELL_W, CELL_W);
+        gc.drawImage(playerImg, pc.getCol()*CELL_W, (s.getHeight()-1-pc.getHgt())*CELL_W);
 
         // Draw guards
         gc.setFill(Color.RED);
         for (Coord g: gCoords) {
-            gc.fillOval(g.getCol()*CELL_W, (s.getHeight()-1-g.getHgt())*CELL_W,
-                        CELL_W, CELL_W);
+            gc.drawImage(guardImg, g.getCol()*CELL_W, (s.getHeight()-1-g.getHgt())*CELL_W);
         }
 
         // Draw treasures
         for (Coord t: tCoords) {
-            if(gCoords.contains(t)) gc.setFill(Color.ORANGE);
-            else gc.setFill(Color.GOLD);
-            gc.fillOval(t.getCol()*CELL_W, (s.getHeight()-1-t.getHgt())*CELL_W,
-                        CELL_W, CELL_W);
+            if(gCoords.contains(t))
+                gc.drawImage(treaGuardImg, t.getCol()*CELL_W, (s.getHeight()-1-t.getHgt())*CELL_W);
+            else
+                gc.drawImage(treasureImg, t.getCol()*CELL_W, (s.getHeight()-1-t.getHgt())*CELL_W);
         }
     }
 
